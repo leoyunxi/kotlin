b8acbdb2fa8ca5b880d28d7489ec0e46ba008d53
leoyunxi <leoyunxi@gmail.com>
2021-03-13 13:04:49 +0900

    1. add kotlin-gradle-plugin & kotlin-stdlib to build.gradle 2. convert .java → .kt

---
+++ app/src/main/java/com/demo/javatokotlin/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.demo.javatokotlin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val person = Person(18, "leon")
+        // Action -> convert java to kotlin
+    }
+}
---
+++ app/src/main/java/com/demo/javatokotlin/Person.kt
@@ -0,0 +1,9 @@
+package com.demo.javatokotlin
+
+/**
+@author yunxi.liu
+@date 2021/03/13
+ */
+class Person (val age:Int,val name:String){
+
+}
